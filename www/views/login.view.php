84de4e9cbcd28c10d0944e4d76f7d12d0db0c589
bastiroid <basti.rosenqvist@gmail.com>
2014-02-03 20:16:18 +0200

    Complete login.view.php styling
    Added style rules for form elements and general layout.
    Override of list elements and merger of list elements

--- www/views/login.view.php
+++ www/views/login.view.php
@@ -1,36 +1,55 @@
-<h1>Sharedoc</h1>
-<h2>Sign in</h2>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<form action="" method="post">
-	<ul>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
 
-		<li>
-			<label for="email">Email address</label>
-			<input type="text" name="email" id="email" value="<?= escape(Input::get('email')) ?>">
-		</li>
-
-		<li>
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password">
-		</li>
-
-		<li>
-			<label for="remember">
-				<input type="checkbox" name="remember" id="remember">
-				Remember me
-			</label>
-			
-		</li>
-
-		<li>
-			<input type="hidden" name="token" value="<?= Token::generate(); ?>">
-			<input type="submit" value="Login">
-		</li>
-	</ul>
-
-	<?php if (isset($status)) : ?>
-		<p><?= $status; ?></p>
-	<?php endif; ?>
-</form>
-
-<p><a href="signup.php">Not a member yet? Sign up here</a></p>
+    <div class="container">
+        <div class="form">
+            <div class="form_header">
+                
+                    <h1>Login To Your Sharedoc</h1>
+                
+            </div>
+            <div id="login" class="form-action show">
+                <form action="" method="post">
+                    <ul>
+                        <li>
+                            <label for="email"></label>
+                            <input type="text" placeholder="Email" name="email" id="email" value="<?= escape(Input::get('email')) ?>"/>
+                        </li>
+                        <li>
+                            <label for="password"></label>
+                            <input type="password" placeholder="Password" />
+                        </li>
+                        <li>
+                            <label for="remember">
+                            <input type="checkbox" name="remember" id="remember">
+                                Remember me
+                            </label>
+                        </li>
+                        <li>
+                            <input type="hidden" name="token" value="<?= Token::generate(); ?>">
+                            <input type="submit" value="Login" class="loginbtn" />
+                        </li>
+                    </ul> 
+                        <?php if (isset($status)) : ?>
+                        <p><?= $status; ?></p>
+                        <?php endif; ?>                
+                </form>
+            </div>
+            <div class="form_footer">
+                
+                    <p><a href="signup.php">Not a member yet? Sign up here</a></p>
+                
+            </div>            
+        </div>
+    </div>
+</body>
+</html>
