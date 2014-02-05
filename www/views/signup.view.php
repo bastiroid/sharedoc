d133f387907b957a682b1183ad4a28ed2463460e
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-02-05 14:00:09 +0200

    signup.view.php and CSS classes
    Fixed signup.view and styled selective css classes

--- www/views/signup.view.php
+++ www/views/signup.view.php
@@ -1,40 +1,44 @@
-<h1>Sharedoc</h1>
-<h2>Register a new account</h2>
+<div class="container">
+        <div class="form2">
+            <div class="form_header">
+                
+                    <h1>Signup for Sharedoc</h1>
+                
+            </div>
+            <div id="login" class="form-action show">
+                <form action="" method="post">
+                    <ul>
+                        <li>
+                            <label for="first_name"></label>
+                            <input type="text" placeholder="First Name" name="first_name" id="first_name" value="<?= escape(Input::get('first_name')) ?>">
+                        </li>
 
-<form action="" method="post">
-	<ul>
-		<li>
-			<label for="first_name">First name</label>
-			<input type="text" name="first_name" id="first_name" value="<?= escape(Input::get('first_name')) ?>">
-		</li>
-
-		<li>
-			<label for="last_name">Last name</label>
-			<input type="text" name="last_name" id="last_name" value="<?= escape(Input::get('last_name')) ?>">
-		</li>
-
-		<li>
-			<label for="email">Email address</label>
-			<input type="text" name="email" id="email" value="<?= escape(Input::get('email')) ?>">
-		</li>
-
-		<li>
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password">
-		</li>
-
-		<li>
-			<label for="pwd_again">Password again</label>
-			<input type="password" name="pwd_again" id="pwd_again">
-		</li>
-
-		<li>
-			<input type="hidden" name="token" value="<?= Token::generate(); ?>">
-			<input type="submit" value="Create new account">
-		</li>
-	</ul>
-
-	<?php if (isset($status)) : ?>
-		<p><?= $status; ?></p>
-	<?php endif; ?>
-</form>
+                        <li>
+                            <label for="last_name"></label>
+                            <input type="text" placeholder="Last Name" name="last_name" id="last_name" value="<?= escape(Input::get('last_name')) ?>">
+                        </li>
+                        <li>
+                            <label for="email"></label>
+                            <input type="text" placeholder="Email" name="email" id="email" value="<?= escape(Input::get('email')) ?>"/>
+                        </li>
+                        <li>
+                            <label for="password"></label>
+                            <input type="password" placeholder="Password" />
+                        </li>
+                        <li>
+                            <label for="password"></label>
+                            <input type="password" placeholder="Password Again" />
+                        </li>
+                        <li>
+                            <input type="hidden" name="token" value="<?= Token::generate(); ?>">
+                            <input type="submit" value="Create your account" class="signupbtn" />
+                        </li>
+                    </ul> 
+                        <?php if (isset($status)) : ?>
+                        <p><?= $status; ?></p>
+                        <?php endif; ?>                
+                </form>
+            </div>
+                        
+        </div>
+    </div>
