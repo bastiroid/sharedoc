11fcd17c45369b1a683cf0d35588698a6b1d7d4a
bastiroid <basti.rosenqvist@gmail.com>
2014-02-25 19:05:55 +0200

    Header & Footer
    Added header with navigation and footer with copyright

--- www/views/layout.php
+++ www/views/layout.php
@@ -10,6 +10,23 @@
 
 	<div class="container">
 		<?php include($path); ?>
+	<div id='header'>
+    <div class='userbox'>
+    	<div id='cssmenu'>
+			<ul>
+   				<li class='has-sub last'><a href='#'><span>Username</span></a>
+      				<ul>
+         				<li><a href='#'><span>Account</span></a></li>
+         				<li class='last'><a href='#'><span>Logout</span></a></li>
+      				</ul>
+   				</li>
+			</ul>
+		</div>
+	</div>
+</div>	
+
+	<div id="footer">
+  		Copyright © <?php echo date("Y"); ?> ShareDoc
 	</div>
 	
 </body>
