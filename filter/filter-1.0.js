c5695c14903b0affafc72f8ca5b2c88c9d2b3611
quentindelattre <hey@quentindelattre.ch>
2014-03-03 19:44:58 +0200

    FULL FILTERING
    The PHP has been commented but everything should be working properly
    with what I check manually from the database on phpMyAdmin and the HTML
    code.

---
+++ filter/filter-1.0.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	// LIST
+	if($('#list').length > 0){
+		$('#filter a').each(function(index, element) {
+            $(this).click(function(){
+				return Filter(this);
+			});
+        });
+		if(window.location.hash){
+			hash = window.location.hash.substr(1);
+			$('#filter a.f-' + hash).each(function(index, element) {
+               return Filter(this);
+            });
+			
+		}
+	}
+});
+
+/**************************************
+FILTER
+**************************************/
+
+function Filter(id){
+	$('#list .doc a').removeAttr('onclick');
+	window.location.hash = 'all';
+	if( $(id).hasClass('f-all') || $(id).hasClass('sel')){
+		$('#filter a').removeClass('sel');
+		$('#list .doc a').parent().stop();
+		$('#list .doc a').parent().show('slow',function(){
+			$('#list .doc a').parent().removeClass('off');
+		});
+		$('#filter a.f-all').addClass('sel');
+	}else{
+		$('#filter a').removeClass('sel');
+		var f_class = $(id).attr('class');
+		var hash = f_class.substr(2);
+		window.location.hash = hash;
+		$('#list .doc a:not(.' + f_class + ')').attr('onclick','return false;');
+		$('#list .doc a:not(.' + f_class + ')').parent().stop();
+		$('#list .doc a:not(.' + f_class + ')').parent().hide('fast',function(){
+			$('#list .doc a:not(.' + f_class + ')').parent().addClass('off');
+		});
+		$('#list .doc a.' + f_class).parent().stop();
+		$('#list .doc a.' + f_class).parent().show('slow',function(){
+			$('#list .doc a.' + f_class).parent().removeClass('off');
+		});
+		$(id).addClass('sel');
+	}
+	return false;
+}
---
+++ filter/filtering.php
@@ -0,0 +1,122 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <title>filter</title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="filter-1.0.js"></script>  
+    <style type="text/css" media="screen">
+    	#main{
+	    	width: 1080px;
+	    	margin: 0 auto;
+    	}
+    	#filter, #list {
+	    	float: left;
+	    	width: 100%;
+    	}
+    	#filter li{
+    		list-style: none;
+	    	float: left;
+	    	margin-right: 15px;
+    	}
+    	#list .doc{
+	    	float: left;
+	    	margin: 15px;
+    	}
+    </style>
+
+</head>
+
+<body>
+   <div id="main">
+    <ul id="filter">
+        <li><a href="#" class="f-all sel">All documents</a></li>
+        
+    	<?php
+    		/*
+			foreach($groups as $group){
+    			// if admin of group ?
+    			// if private group ?
+	    		echo('<li><a href="#" class="f-'.get group id.'">'.get group name.'</a></li>');
+    		}
+*/
+    	?>
+
+        <li><a href="#" class="f-personnal">Personnal documents</a></li>
+
+        <li><a href="#" class="f-group1">Group 1</a></li>
+
+        <li><a href="#" class="f-group2">Group 2</a></li>
+
+        <li><a href="#" class="f-group3">Group 3</a></li>
+
+        <li><a href="#" class="f-group4">Group 4</a></li>
+
+        <li><a href="#" class="f-group5">Group 5</a></li>
+    </ul>
+
+    <div id="list">
+    	<?php
+    		/*
+    		foreach($documents as $document){
+	    		echo('
+	    			<div class="doc">
+			            <a href="#" class="f-'.get group id.' "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+			        </div>
+	    		');
+    		}
+    		*/ 
+    	?>
+        <div class="doc">
+            <a href="#" class="f-group1 f-group4 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group1 f-personnal "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group5 f-group3 f-personnal f-group4 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group5 f-group1 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group3 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group3 f-group2 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group1 f-personnal "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group3 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group3 f-group4 f-group2 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group5 f-group3 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+
+        <div class="doc">
+            <a href="#" class="f-group5 f-group3 f-group4 "><img src="http://placehold.it/200x250&text=document+placeholder"></a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
