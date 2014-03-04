257c112971c0f98cb0253d397a8b3912917b026c
quentindelattre <hey@quentindelattre.ch>
2014-03-04 11:26:43 +0200

    Corrected Document SQL Query

--- filter/sql_requests.sql
+++ filter/sql_requests.sql
@@ -17,8 +17,9 @@ WHERE `ipp_group_join`.`user_id` = /* Current user id */
 AND `ipp_group`.`is_shared` = TRUE;
 
 
-SELECT `ipp_document`.`group_id`,`ipp_document`.`name`,`ipp_document`.`id`
+SELECT DISTINCT `ipp_document`.`group_id`,`ipp_document`.`name`,`ipp_document`.`id`
 FROM `ipp_document`
-INNER JOIN `ipp_users`
-ON `ipp_document`.`admin_id`= `ipp_users`.`id`
-WHERE `ipp_document`.`admin_id` = /*current user id*/;
+WHERE `ipp_document`.`group_id` IN (
+	SELECT `ipp_group_join`.`group_id`
+	FROM `ipp_group_join`
+	WHERE `ipp_group_join`.`user_id` = /*current user id*/);
