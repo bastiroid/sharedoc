888a520654264246bd48043432eed3271826db77
bastiroid <basti.rosenqvist@gmail.com>
2014-03-10 21:00:19 +0200

    Added Sidebar navigation
    Added sidebar container in style.css and sidebar html in header.php.
    Added component.css and demo.css in layout.php.
    Added accompanying JS for sidebar transitions.
    Added icon styles for sidebar

--- www/views/header.php
+++ www/views/header.php
@@ -12,4 +12,29 @@
 				</ul>
 			</div>
 		</div>
+		<div class='sidebar'>
+            <ul id="gn-menu" class="gn-menu-main">
+            	<li class="gn-trigger"><a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+            		<nav class="gn-menu-wrapper">
+                    	<div class="gn-scroller">
+                		<ul class="gn-menu">
+                  			<li class="gn-search-item"><input placeholder="Search" type="search" class="gn-search">
+                    		<a class="gn-icon gn-icon-search"><span>Search</span></a>
+                  			</li>
+                  			<li><a class="gn-icon gn-icon-archive">Folder 1</a></li>
+                  			<li><a class="gn-icon gn-icon-archive">Folder 2</a></li>
+                  			<li><a class="gn-icon gn-icon-archive">Folder 3</a></li>
+                  			<li><a class="gn-icon gn-icon-archive">Folder 4</a></li>
+                		</ul>
+                		<div class="gn-scroller-footer">© Copyright<?php echo date("Y"); ?> ShareDoc</div>
+              			</div><!-- /gn-scroller -->
+            		</nav>
+          		</li>    
+            </ul>
+        </div>
 	</div>
+	<script src="js/classie.js"></script>
+    <script src="js/gnmenu.js"></script>
+    <script>
+      new gnMenu( document.getElementById( 'gn-menu' ) );
+    </script>
--- www/views/layout.php
+++ www/views/layout.php
@@ -7,7 +7,10 @@
 	<link rel="stylesheet" href="css/style.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-    <script type="text/javascript" src="js/filter-1.0.js"></script> 
+    <script type="text/javascript" src="js/filter-1.0.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+    <script src="js/modernizr.custom.js"></script> 
 </head>
 <body>
 	
