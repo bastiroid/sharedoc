95de5fe9c2736b517b8148f0e72b2788611e4470
quentindelattre <hey@quentindelattre.ch>
2014-03-10 19:19:49 +0200

    Adapt filter to index.view.php

--- www/views/index.view.php
+++ www/views/index.view.php
@@ -1,21 +1,25 @@
 <?php
 
-var_dump($data); ?>
+//var_dump($data); ?>
 
-<ul>
+<ul id="filter">
+<li><a href="#" class="f-all sel">All documents</a></li>
 <?php if($data['groups']) foreach ($data['groups'] as $group): ?>
 
-	<li><?= $group->name ?></li>
+	<li><a href="#" class="f-<?= $group->id ?>"><?= $group->name ?></a></li>
 
 <?php endforeach; ?>
 </ul>
 
 <br><br><br>
 
-<ul>
+<ul id="list">
+	<li class="create_doc">
+		<a href="#" class="get_selected_group_class"><img src="http://placehold.it/200x250&text=create+document"></a>
+    </li>
 <?php if($data['documents']) foreach ($data['documents'] as $document): ?>
 
-	<li><?= $document->name ?></li>
+	<li><a href="#" class="<?= $document->id ?>"><?= $document->name ?></a></li>
 
 <?php endforeach; ?>
 </ul>
