f0c64fe514cfafea402291d5df569f7e5e77030d
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-03-11 20:11:40 +0200

    added link to index.php

--- www/views/header.php
+++ www/views/header.php
@@ -3,7 +3,7 @@
 	    <div class='userbox'>
 	    	<div id='cssmenu'>
 				<ul>
-	   				<li class='has-sub last'><a href='#'><span><?php echo $data['user']->first_name; ?></span></a>
+	   				<li class='has-sub last'><a href='index.php'><span><?php echo $data['user']->first_name; ?></span></a>
 	      				<ul>
 	         				<li><a href='settings.php'><span>Change Password</span></a></li>
 	         				<li class='last'><a href='logout.php'><span>Logout</span></a></li>
