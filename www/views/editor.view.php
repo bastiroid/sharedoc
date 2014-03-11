e886acd1cb2ea7a73af806cd5397912b1436a1f2
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-03-11 19:08:25 +0200

    added editor.view.php

---
+++ www/views/editor.view.php
@@ -0,0 +1,12 @@
+<div class="editor">
+<form>
+            <textarea name="editor1" id="editor1" rows="40" cols="145">
+                Documents content comes here
+            </textarea>
+            <script>
+                // Replace the <textarea id="editor1"> with a CKEditor
+                // instance, using default configuration.
+                CKEDITOR.replace( 'editor1' );
+            </script>                
+</form>
+</div>
