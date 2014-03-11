2003e7301c931985a5ad23f86f6bdecf05ad8867
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-03-11 18:51:44 +0200

    small changes to change password

--- www/views/settings.view.php
+++ www/views/settings.view.php
@@ -2,20 +2,20 @@
     <form action="" method="post">
         <ul>
             <li>
-                <label for="password_current">Current password</label>
+                <label for="password_current"></label>
                 <input type="password" placeholder="Current password" name="password_current" id="password_current" />
             </li>
             <li>
-                <label for="password_new">New password</label>
+                <label for="password_new"></label>
                 <input type="password" placeholder="New password" name="password_new" id="password_new" />
             </li>
             <li>
-                <label for="password_new_again">New password again</label>
+                <label for="password_new_again"></label>
                 <input type="password" placeholder="New password again" name="password_new_again" id="password_new_again" />
             </li>
             <li>
                 <input type="hidden" name="token" value="<?= Token::generate(); ?>">
-                <input type="submit" value="Change password" class="loginbtn" />
+                <input type="submit" value="Change password" class="pswbtn" />
             </li>
         </ul> 
         <?php if (isset($status)) : ?>
