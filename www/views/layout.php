4c2dcfb0b74dcd51282f427218b7fe806f69f15e
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-03-14 22:12:41 +0200

    optimizing document loading order

--- www/views/layout.php
+++ www/views/layout.php
@@ -5,11 +5,11 @@
 	<title>Sharedoc</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="css/style.css">
-	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-    <script type="text/javascript" src="js/filter-1.0.js"></script>
     <link rel="stylesheet" type="text/css" href="css/demo.css" />
     <link rel="stylesheet" type="text/css" href="css/component.css" />
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/filter-1.0.js"></script>
     <script src="js/modernizr.custom.js"></script>
     <script src="ckeditor/ckeditor.js"></script> 
 </head>
