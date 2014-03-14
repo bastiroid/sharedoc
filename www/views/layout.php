dd6c6dc78cc53e7f1f0226f33e1da32cca1cee5e
Sebastian Rosenqvist <basti.rosenqvist@gmail.com>
2014-03-14 22:16:52 +0200

    removed unused stylesheet

--- www/views/layout.php
+++ www/views/layout.php
@@ -5,7 +5,6 @@
 	<title>Sharedoc</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/demo.css" />
     <link rel="stylesheet" type="text/css" href="css/component.css" />
     <script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
